fa37a2ee34abf98334d481076f06a45f1bac385e
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2017-06-15 15:06:08 +0200

    Speakers (#2)
    * first init for speakers view
    
    * speaker-list from fake ajax
    
    * bedre lesbarhet på speakers
    
    * riktige ikoner
    
    * menyikon-justering
    
    * justering av ikoner
    
    * enda mer jokking på de derre ikonene
    
    * Oooooog bittelitt til
    
    * Flytta meetups-ikoner
    
    * midtstilt speakers
    
    * Inkludert es6-convertering for litt niceness
    
    * Ny tekst
    
    * fps-sjekk på canvas
    
    * fallback til bilde ved lav fps
    
    * Litt bedre overgang fra canvas til fallback
    
    * Litt færre partikler
    
    * Riktig alfabetisk rekkefølge
    
    * linjehøyde
    
    * bg-fix til iphone
    
    * repeterende bg til telefon
    
    * fake ajax for now
    
    * crop på bildet
    
    * Ny look for speakers på mobil

--- js/main.js
+++ js/main.js
@@ -23,9 +23,9 @@ var scrollHelper = {
 			var position = scrollHelper.easeInOut(elapsedTime, start, change, duration);
 			window.scrollTo(0, position);
 			if (elapsedTime < duration) {
-				setTimeout(function() {
+				requestAnimationFrame(function() {
 					animateScroll(elapsedTime);
-				}, increment);
+				});
 			}
 		};
 
@@ -111,7 +111,9 @@ var loadJSONP = (function(){
 		},
 		window_width = window.innerWidth,
 		logo = _si('.logo'),
-		logoTop = logo.getBoundingClientRect().top + scrollTop;
+		logoTop = logo.getBoundingClientRect().top + scrollTop,
+		lowFpsCount = 0,
+		startup = Date.now();
 
 	function render() {
 		if (scrollTop !== oldScrollTop) {
@@ -126,13 +128,13 @@ var loadJSONP = (function(){
 				html.classList.remove('toggle--logo-in-menu');
 			}
 
-			for (var i = sectionTops.length; i--;) {
+			for (let i = sectionTops.length; i--;) {
 				if (scrollTop > sectionTops[i]) {
-					var li = _s('nav li')[i];
+					let li = _s('nav li')[i];
 					if (!li.classList.contains('active')) {
 						_si('nav li.active').classList.remove('active');
 						li.classList.add('active');
-						var url = _si('a', li).href;
+						let url = _si('a', li).href;
 						url = url.substr(url.indexOf('#'));
 						location.hash = url;
 					}
@@ -158,11 +160,11 @@ var loadJSONP = (function(){
 	});
 
 	_ael('nav', 'click', function(e) {
-		var li = e.target.closest('li');
+		let li = e.target.closest('li');
 		if (li) {
-			var url = _si('a', li).href;
+			let url = _si('a', li).href;
 			url = url.substr(url.indexOf('#') + 1);
-			var section = _si('[data-link-url="' + url + '"]');
+			let section = _si('[data-link-url="' + url + '"]');
 			scrollHelper.scrollTo(section.getBoundingClientRect().top + scrollTop + 1, 300);
 			html.classList.remove('show--menu');
 		}
@@ -176,7 +178,366 @@ var loadJSONP = (function(){
 		_si('nav a[href*="' + location.hash + '"]').click();
 	}
 
+	_ael('.block--conduct .toggle-extra', 'click', function(e) {
+		e.preventDefault();
+		this.closest('.block').classList.toggle('open');
+	});
+
+	/* ekte ajax *
+	_ajax('https://api.trondheimdc.no/events/tdc2017/sessions', function() {
+		loadSpeakers(JSON.parse(data.responseText));
+	});
+	/* */
+	function loadSpeakers(speakers) {
+		var fallbackImg = '//placehold.it/360x240/117fe8/fff';
+		var listHtml = '';
+		speakers.forEach(sessh => {
+			var img = sessh.foredragsholdere[0].bildeUri || fallbackImg;
+			var name = sessh.foredragsholdere[0].navn;
+			var title = sessh.tittel;
+			listHtml += `<li>
+							<img src="${img}">
+							<div>
+								<h5>${name}</h5>
+								<p>${title}</p>
+							</div>
+						</li>`;
+		});
+
+		_si('.block--speakers ul').innerHTML = listHtml;
+	}
+
+	/* fake ajax */
+	loadSpeakers([
+		{
+			"tittel": "Do or don't. There's no try. Or is there? The power of monads explained. Sort of",
+			"foredragsholdere": [
+			{
+				"navn": "Sander Hoogendoorn",
+				"bildeUri": "https://static.trondheimdc.no/2017/Sander-Hoogendoorn.jpg"
+			}
+			]
+		},
+		{
+			"tittel": "Getting started with Java 9 modules",
+			"foredragsholdere": [
+			{
+				"navn": "Rafael Winterhalter",
+				"bildeUri": "https://secure.gravatar.com/avatar/bc96521f79789d75fa63cfa5c2758765?s=180"
+			}
+			]
+			},
+		{
+			"tittel": "Continuously delivering continuous delivery",
+			"foredragsholdere": [
+			{
+				"navn": "Kim van Wilgen",
+				"bildeUri": "https://secure.gravatar.com/avatar/32f9be308b887028f42c6baa4235fc5c?s=180"
+			}
+			]
+		},
+		{
+			"tittel": "In a Nutshell: Immutable Objects in Java",
+			"foredragsholdere": [
+			{
+				"navn": "Marcus Biel",
+				"bildeUri": "https://static.trondheimdc.no/2017/Marcus_Biel.png"
+			}
+			]
+		},
+		{
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Martha Eike",
+				"bildeUri": "https://static.trondheimdc.no/uploads/2015/09/Martha_Eike-Portrett.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Tarjei Vassbotn Sigve Tjora",
+				"bildeUri": "https://static.trondheimdc.no/2017/tarjei-vassbotn.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Brian Christian",
+				"bildeUri": "https://static.trondheimdc.no/2017/brian-christian.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Preethi Kasireddy",
+				"bildeUri": "https://static.trondheimdc.no/2017/Preethi_Kasireddy.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Jo-Remi Madsen",
+				"bildeUri": "https://static.trondheimdc.no/2017/Jo-Remi_Madsen.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Eirik Backer",
+				"bildeUri": "https://static.trondheimdc.no/2017/Eirik_Backer.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Axel Tideman",
+				"bildeUri": "https://static.trondheimdc.no/2017/Axel_Tideman.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Bjørn Borud",
+				"bildeUri": "https://static.trondheimdc.no/2017/Bjorn_Borud.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Berit Alvestrand",
+				"bildeUri": "https://static.trondheimdc.no/2017/Berit_Alvestrand.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Jonas Follesø",
+				"bildeUri": "https://static.trondheimdc.no/2017/Jonas_Folleso.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Øystein Askeland",
+				"bildeUri": "https://static.trondheimdc.no/2017/oystein_Askeland.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Erik Engheim",
+				"bildeUri": "https://static.trondheimdc.no/2017/Erik_Engheim.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Robert Engels",
+				"bildeUri": "https://static.trondheimdc.no/2017/Robert_Engels.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Robert Virding",
+				"bildeUri": "https://static.trondheimdc.no/2017/Robert_Virding.jpg"
+			}
+			]
+		}, {
+			"tittel": "",
+			"foredragsholdere": [
+			{
+				"navn": "Francis Paulin",
+				"bildeUri": "https://static.trondheimdc.no/2017/Francis_Paulin.jpg"
+			}
+			]
+		}, {
+		"tittel": "",
+		"foredragsholdere": [
+			{
+			"navn": "Mozhgan Tavakolifard",
+			"bildeUri": "https://static.trondheimdc.no/2017/Mozhgan_Tavakolifard.jpg"
+			}
+		]
+		} 
+		]);
+
 	loadJSONP('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=00622ccfe6e4d518ca49b0b5105abb54&per_page=20&user_id=trondheimdc&tags=Approved&page=2&extras=o_dims&format=json');
+
+	function Mouse(element, callback) {
+		this.callback = callback;
+		this.element = element;
+		this.handler = this.handler.bind(this);
+		this.L = this.M = this.R = this.X = this.Y = 0;
+		window.addEventListener('mousedown', this.handler);
+		window.addEventListener('mouseup', this.handler);
+		window.addEventListener('mousemove', this.handler);
+	}
+
+	Mouse.prototype.map = { 0: 'L', 1: 'M', 2: 'R' };
+
+	Mouse.prototype.handler = function(e) {
+		var b = this.element.getBoundingClientRect();
+		this.X = e.clientX - b.left;
+		this.Y = e.clientY - b.top;
+		switch(e.type) {
+			case 'contextmenu': e.preventDefault(); break;
+			case 'mousedown': this[this.map[e.button]] = 1; break;
+			case 'mouseup': this[this.map[e.button]] = 0; break;
+		}
+		this.callback && this.callback(e);
+	};
+
+	function Particle(field, x, y) {
+		this.field = field;
+		this.l = vec4.get(x, y);
+		this.p = vec4.get(x, y);
+		this.v = vec4.get();
+	}
+
+	Particle.prototype.reset = function(x, y) {
+		if(x == null || y == null) if(Math.random() < 0.5) {
+			x = this.field.width  * (Math.random());
+			y = this.field.height * (Math.random() + 0.5 | 0);
+		} else {
+			x = this.field.width  * (Math.random() + 0.5 | 0);
+			y = this.field.height * (Math.random());
+		}
+
+		vec4.set(this.l, x, y);
+		vec4.set(this.p, x, y);
+		vec4.set(this.v);
+	};
+
+	Particle.prototype.outOfBounds = function() {
+		return this.p[0] < 0 || this.p[0] > this.field.width
+			|| this.p[1] < 0 || this.p[1] > this.field.height;
+	};
+
+	Particle.prototype.update = function() {
+		if(this.outOfBounds()) return;
+
+		var x = 0.00550 * this.p[0];
+		var y = 0.00550 * this.p[1];
+		var z = 0.0001 * this.field.now;
+		var r = Math.random() * 0.5;
+		var t = Math.random() * Math.PI * 2;
+
+		vec4.set(vec4.buffer,
+			r * Math.sin(t) + this.field.simplex.noise3D(x, y, +z),
+			r * Math.cos(t) + this.field.simplex.noise3D(x, y, -z)
+		);
+		vec4.add(this.v, vec4.buffer, this.v);
+
+		if(this.field.mouse.L) {
+			vec4.set(vec4.buffer, this.field.mouse.X, this.field.mouse.Y);
+			vec4.sub(vec4.buffer, this.p, vec4.buffer);
+			vec4.mul(vec4.buffer, 0.0010, vec4.buffer);
+			vec4.add(this.v, vec4.buffer, this.v);
+		}
+
+		vec4.mul(this.v * 0.5, 0.9500, this.v * 0.5);
+		vec4.set(this.l, this.p, this.l);
+		vec4.add(this.p, this.v, this.p);
+
+		return true;
+	};
+
+	function Field(container) {
+		this.loop      = this.loop.bind(this);
+		this.canvas    = util.tag('canvas', null, container);
+		this.info      = util.tag('code',   null, container);
+		this.context   = this.canvas.getContext('2d');
+		this.mouse     = new Mouse(this.canvas);
+		this.simplex   = new SimplexNoise();
+		this.particles = [];
+		this.loop();
+	}
+
+	Field.prototype.spawn = function() {
+		for(var i = Math.round(1e4 / 1.5) - this.particles.length; i--;)
+			this.particles.push(new Particle(this));
+	};
+
+	Field.prototype.resize = function() {
+		var w = this.canvas.clientWidth;
+		var h = this.canvas.clientHeight;
+		if(this.canvas.width  !== w
+		|| this.canvas.height !== h) {
+			this.width  = this.canvas.width  = w;
+			this.height = this.canvas.height = h;
+			this.clear();
+		}
+	};
+
+	Field.prototype.clear = function() {
+		this.context.fillStyle = util.color.rgba(1, 1, 1);
+		this.context.fillRect(0, 0, this.width, this.height);
+	};
+
+	Field.prototype.render = function() {
+		this.context.beginPath();
+
+		for(var p, i = 0; p = this.particles[i++];) if(p.update()) {
+			this.context.moveTo(p.l[0], p.l[1]);
+			this.context.lineTo(p.p[0], p.p[1]);
+		} else p.reset(); // this.particles.splice(--i, 1);
+
+		this.context.globalCompositeOperation = 'lighter';
+		this.context.strokeStyle = util.color.rgba(0.61, 0.76, 0.10, 0.25);
+		this.context.stroke();
+
+		this.context.globalCompositeOperation = 'source-over';
+		this.context.fillStyle = util.color.rgba(0, 0, 0, 0.05);
+		this.context.fillRect(0, 0, this.width, this.height);
+	};
+
+	Field.prototype.update = function() {
+		this.resize();
+		this.spawn();
+		this.render();
+	};
+
+	Field.prototype.loop = function() {
+		this.now = Date.now();
+		if (this.now - startup < 5000) {
+			let fps = util.fps(true);
+			if (fps < 20) {
+				lowFpsCount++;
+				if (lowFpsCount > 60) {
+					html.classList.add('lowfps');
+					setTimeout(function() {
+						document.body.removeChild(_si('canvas'));
+					}, 500);
+					return;
+				}
+			} else {
+				lowFpsCount = 0;
+			}
+		}
+		requestAnimationFrame(this.loop);
+		this.update();
+	};
+
+	window.addEventListener('load', function () {
+		if (window_width > 600) {
+			new Field(document.body);
+		}
+	}, false);
 
 	html.classList.remove('no-js');
 	if ('ontouchstart' in document) {
@@ -186,22 +547,3 @@ var loadJSONP = (function(){
 	}
 
 })();
-
-var firstPhotoWallLoaded = false;
-function jsonFlickrApi(data) {
-	var html = '';
-	counter = 0;
-	data.photos.photo.forEach(function(photo) {
-		if (counter > 8 || photo.o_height - 0 > photo.o_width - 0) { return; }
-		var url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_c.jpg';
-		html += '<li><img src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%20400%20267%27%2F%3E" data-src="' + url + '" class="lazyload"></li>';
-		counter++;
-	});
-	if (firstPhotoWallLoaded) {
-		_s('.block--photos ul')[1].innerHTML = html;
-	} else {
-		firstPhotoWallLoaded = true;
-		_s('.block--photos ul')[0].innerHTML = html;
-		loadJSONP('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=00622ccfe6e4d518ca49b0b5105abb54&per_page=20&user_id=trondheimdc&tags=developer&page=2&extras=o_dims&format=json');
-	}
-}
